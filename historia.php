67fc9fce95600618269f595fca25c5af8ffb2bac
WarriorWave <davidhhw24@gmail.com>
2018-02-24 15:07:08 -0600

    flex box arreglado

--- historia.php
+++ historia.php
@@ -5,8 +5,10 @@
 		<section class="page-one">
 			<article class="init">
 				<img src="img/triforce.png" alt="triforce">
-				<p>
-					
+				<p class="intro">
+					Hace mucho tiempo existió un reino que ocultaba el poder dorado de los dioses. Era un lugar hermoso, de verdes bosques y enormes montañas, donde reinaba la paz.
+
+					Pero un día fue presa de la ambición de un villano que le arrebató el poder dorado. El reino cayó en las tinieblas. Y cuando el pueblo había perdido casi toda esperanza... apareció como de la nada un joven vestido con ropas verdes. Con su misteriosa espada, el joven sepultó al villano y le devolvió al reino la luz. Como venía de otra época, el pueblo le llamó 'Héroe del Tiempo'.
 				</p>
 			</article>
 			<div class="era-one">
